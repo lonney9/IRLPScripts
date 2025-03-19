fed57064db040d9efbd129d56b9cf53bb735bbfa
Lonney <lonney9@icloud.com>
2025-03-18 16:24:55 -0800

    Update setup comments in simplex tail

--- simplex_tail.sh
+++ simplex_tail.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # IRLP Node script to detect COS INACTIVE and key the transmitter
 
+# Setup
+# Save script to /home/irlp/custom/simplex_tail.sh
+# Test from console: ./custom/simplex_tail.sh
+# Start on boot:
+#   1) Newer systems edit /home/irlp/custom/rc.irlp.repeater and add two lines to the end:
+#      echo -n "Starting simplex tail script"
+#      "${CUSTOM}/simplex_tail.sh" >&/dev/null 2>&1 &
+#   2) Older systems (with out rc.irlp.repeater) edit /home/irlp/custom/rc.irlp and add two lines above the last line with "[ DONE ]":
+#      echo -n "Starting simplex tail script"
+#      /bin/su - -c "${CUSTOM}/simplex_tail.sh" repeater >&/dev/null 2>&1 &
+
 # Paths to IRLP commands
 READINPUT_CMD="/home/irlp/bin/readinput"
 FORCEKEY_CMD="/home/irlp/bin/aux1on"
